2e499413948a4d6832adee3cd78da66a54ea08de
petso-r <rohamarazafiarivony@gmail.com>
2024-08-07 17:51:12 +0300

    Bouton ajouter succesfully

--- src/pages/pageEnseignant.js
+++ src/pages/pageEnseignant.js
@@ -1,8 +1,18 @@
 import TableauEnseignant from "../components/enseignantTableau"
 import NavControl from "../components/NavigationControl"
+import { FaPlusCircle } from "react-icons/fa"
+import { useState } from "react"
 
 const EnseignantTabPage = ()=>{
-    return <div>
+
+    const [ajouter, setAjouter] = useState(false)
+    return  <div>
+         <div className="page-control">
+            <button className="add-btn" onClick={()=>{setAjouter(true)}}>
+                <span>Ajouter</span>
+                <FaPlusCircle/> 
+            </button>
+        </div>
         <TableauEnseignant/>
     </div>
 }
