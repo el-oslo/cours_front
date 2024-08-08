00428caeecc2ac113b84fbfd94b9edb23dcd260c
petso-r <rohamarazafiarivony@gmail.com>
2024-08-08 15:47:55 +0300

    ty mo zany affiche teo akaikin le login , kkkk .somary ratsiratsy izy ty ,kkkk

---
+++ src/components/Affichage.js
@@ -0,0 +1,25 @@
+import'./Affichage.css';
+
+const Affichage = ()=>{
+    return(
+        <div className="container-affichage">
+            <div>
+                <h1>Administrateur ?</h1>
+                <p>REmplisez les information</p> 
+            </div>
+            <div>
+                <h2>Gerez les cours et soyez gentil</h2>
+                <p>Les Administrateur sont des personnes prealablement enregistrer</p>
+                <p>Si vous n' avez pas vos cles administrateur</p>
+                <p>Veuillez contacter le departement de l'informatique</p>
+
+            </div>
+            <div>
+                <p>+261338160031</p>
+            </div>
+                 
+        </div>
+    );
+}
+
+export default Affichage;
